2c7294f2734448388835ab1fca618925746e2668
Dinesh19493 <dinesh.reddy.podhigari@gmail.com>
2020-12-06 06:00:11 -0500

    Commit #01
    -- Created new project.

---
+++ customer-mgmt-bvs/src/main/java/com/customer/mgmt/CustomerMgmtBvsApplication.java
@@ -0,0 +1,13 @@
+package com.customer.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMgmtBvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMgmtBvsApplication.class, args);
+	}
+
+}
---
+++ customer-mgmt-bvs/src/test/java/com/customer/mgmt/CustomerMgmtBvsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customer.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMgmtBvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
